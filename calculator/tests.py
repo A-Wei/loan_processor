616cd5dc111c8d97770ae6a23732c4f77ec990f9
Wei Wei <wei@hem.com>
2019-09-05 20:48:17 +0200

    Add more tests

--- calculator/tests.py
+++ calculator/tests.py
@@ -20,7 +20,7 @@ class New_Loan_Test(unittest.TestCase):
 
         self.assertEqual(self.initialized_loan.content.decode("utf-8"), msg)
 
-    def test_new_payment(self):
+    def test_new_payment_success(self):
         data = {
         	"payment": 10000,
 	        "payment_date": "2019-10-10"
@@ -34,13 +34,59 @@ class New_Loan_Test(unittest.TestCase):
 
         self.assertEqual(resp.content.decode("utf-8"), msg)
 
+    def test_new_payment_date_earlier_than_loan_start_date(self):
+        data = {
+        	"payment": 10000,
+	        "payment_date": "2019-08-10"
+        }
+        resp = self.client.post(
+            "/api/v1/new_payment/",
+            data=data,
+            content_type="application/json",
+        )
+        msg = '{"error":"Date is earlier than loan start date!"}'
+
+        self.assertEqual(resp.content.decode("utf-8"), msg)
+
+    def test_new_payment_amount_exceed_remaining_amount(self):
+        data = {
+        	"payment": 50000,
+	        "payment_date": "2019-09-10"
+        }
+        resp = self.client.post(
+            "/api/v1/new_payment/",
+            data=data,
+            content_type="application/json",
+        )
+        msg = '{"error":"Payment amount exceeds remaining amount!"}'
+
+        self.assertEqual(resp.content.decode("utf-8"), msg)
+
 
     def test_get_balance(self):
         resp = self.client.get(
             "/api/v1/get_balance/?request_date=2019-10-11",
             content_type="application/json",
         )
-        msg = '{"Remaining Amount":40219.17808219178}'
+        msg = '{"Remaining Amount":40219.18}'
+
+        self.assertEqual(resp.content.decode("utf-8"), msg)
+
+    def test_get_balance_deduct_payment(self):
+        data = {
+        	"payment": 10000,
+	        "payment_date": "2019-09-10"
+        }
+        self.client.post(
+            "/api/v1/new_payment/",
+            data=data,
+            content_type="application/json",
+        )
+        resp = self.client.get(
+            "/api/v1/get_balance/?request_date=2019-10-11",
+            content_type="application/json",
+        )
+        msg = '{"Remaining Amount":30219.18}'
 
         self.assertEqual(resp.content.decode("utf-8"), msg)
 
--- calculator/views.py
+++ calculator/views.py
@@ -8,7 +8,7 @@ current_loan_obj = None
 
 
 # Create your views here.
-class Calculator():
+class Loan():
     def __init__(self, initial_amount, interest_rate, start_date):
         self.principal_amount = initial_amount
         self.interest_rate = interest_rate
@@ -20,57 +20,53 @@ class Calculator():
 
     def add_payment(self, amount, payment_date):
         try:
-            # import ipdb; ipdb.set_trace()
             if self.get_balance(payment_date) < amount:
-                return "Amount exceeds remaining amount!"
+                return "Payment amount exceeds remaining amount!"
             self.total_amount_paid += amount
             payment_date_obj = datetime.strptime(payment_date, "%Y-%m-%d")
             self.payment_records.append((payment_date_obj, amount))
             self.payment_records.sort()
             return "Success"
         except Exception as e:
-            print(str(e))
             return str(e)
 
     def get_balance(self, given_date):
         try:
             given_date = datetime.strptime(given_date, "%Y-%m-%d")
+            days_passed = (given_date - self.start_date).days
+            interest_amount = float(self.principal_amount * self.daily_interest_rate) * days_passed
+            payment_sum = 0
+
             if given_date < self.start_date:
-                print("Exception : Requested date is earlier than start date")
-                raise Exception("Requested date is earlier than start date!")
+                raise Exception("Date is earlier than loan start date!")
             elif len(self.payment_records) == 0 or given_date > self.payment_records[-1][0]:
-                days_passed = (given_date - self.start_date).days
-                interest_amount = float(self.principal_amount * self.daily_interest_rate) * days_passed
                 total_payable = self.principal_amount + interest_amount
-                print(total_payable)
-                # total_amount_paid = payments.filter
                 remaining_balance = total_payable - self.total_amount_paid
-                return remaining_balance
+                return round(remaining_balance,2)
             else:
-                payment_sum = 0
-                days_passed = (given_date - self.start_date).days
-                interest_amount = float(self.principal_amount * self.daily_interest_rate) * days_passed
                 total_payable = self.principal_amount + interest_amount
                 for payment in self.payment_records:
                     if payment[0] <= given_date:
                         payment_sum += payment[1]
                 remaining_balance = total_payable - payment_sum
-                return remaining_balance
+                return round(remaining_balance,2)
         except Exception as e:
-            return str(e)
+            raise e
 
 
 # initial new loan
 @api_view(["POST"])
 def new_loan(request):
+    """API endpoint for register new loan"""
     principal_amount = request.data["amount"]
     interest_rate = request.data["interest_rate"]
-    loan_date = request.data["start_date"]
+    start_date = request.data["start_date"]
 
     global current_loan_obj
-    current_loan_obj = Calculator(principal_amount, interest_rate, loan_date)
+    current_loan_obj = Loan(principal_amount, interest_rate, start_date)
 
     return Response({"message":"new loan initiated"})
+
 
 @api_view(["POST"])
 def new_payment(request):
@@ -83,7 +79,11 @@ def new_payment(request):
         else:
             return Response({"error": payment_status}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        return Response({"error": "Loan not initialized!"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"error": "Loan not initialized!"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 @api_view(["GET"])
 def get_balance(request):
@@ -92,4 +92,7 @@ def get_balance(request):
         balance = current_loan_obj.get_balance(request_date)
         return Response({"Remaining Amount": balance})
     else:
-        return Response({"error": "Loan not initialized!"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"error": "Loan not initialized!"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
